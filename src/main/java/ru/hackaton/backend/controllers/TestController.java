b80b04d21098b55d88d1c5b0f3d9e40dfc1c8c47
Damir <rbk_74@mail.ru>
2023-05-26 12:17:29 +0300

    add count and sum methods for tests and change art filter to input number instead of string

--- src/main/java/ru/hackaton/backend/controllers/TestController.java
+++ src/main/java/ru/hackaton/backend/controllers/TestController.java
@@ -46,6 +46,15 @@ public interface TestController {
     @ResponseStatus(HttpStatus.OK)
     List<TestDto> readAll(@RequestParam(value = "page", defaultValue = "0", required = false) Integer pageNum,
                           @RequestParam(value = "per_page", defaultValue = "25", required = false) Integer perPage,
-                          @Parameter(description = "Сложность теста (LITE, INTERMEDIATE, HARD)") @RequestParam(value = "difficulty", required = false) Difficulty difficulty);
+                          @Parameter(description = "Сложность теста (LITE, INTERMEDIATE, HARD)") @RequestParam(value = "difficulty", required = false) Difficulty difficulty,
+                          @RequestParam(value = "art_id", required = false) Long artId);
+
+    @GetMapping(value = "/count", produces = APPLICATION_JSON_VALUE)
+    @ResponseStatus(HttpStatus.OK)
+    Long getCount(@RequestParam(value = "art_id", required = false) Long artId);
+
+    @GetMapping(value = "/score-sum", produces = APPLICATION_JSON_VALUE)
+    @ResponseStatus(HttpStatus.OK)
+    Long getScoreSum(@RequestParam(value = "art_id", required = false) Long artId);
 
 }
--- src/main/java/ru/hackaton/backend/repositories/TestRepository.java
+++ src/main/java/ru/hackaton/backend/repositories/TestRepository.java
@@ -6,8 +6,11 @@ import org.springframework.data.jpa.domain.Specification;
 import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.lang.NonNull;
 import org.springframework.stereotype.Repository;
+import ru.hackaton.backend.models.domain.Art_;
 import ru.hackaton.backend.models.domain.Difficulty;
 import ru.hackaton.backend.models.domain.Test;
 import ru.hackaton.backend.models.domain.Test_;
@@ -29,12 +32,23 @@ public interface TestRepository extends JpaRepository<Test, Long>, JpaSpecificat
     @EntityGraph(attributePaths = {"art"})
     Page<Test> findAll(Specification<Test> spec, @NonNull Pageable pageable);
 
+    @Query(value = "SELECT SUM(t.scorePerQuestion * size(t.questions)) FROM Test t WHERE t.art.id = :art_id")
+    Long findScoreSum(@Param("art_id") Long artId);
+
+    @Query(value = "SELECT SUM(t.scorePerQuestion * size(t.questions)) FROM Test t")
+    Long findScoreSum();
+
 
     interface Specs {
 
         static Specification<Test> difficultyEquals(Difficulty difficulty) {
             return (root, query, builder) -> builder.equal(root.get(Test_.DIFFICULTY), difficulty);
         }
+
+        static Specification<Test> artIdEquals(Long artId) {
+            return (root, query, builder) -> builder.equal(root.get(Test_.ART).get(Art_.ID), artId);
+        }
+
 
     }
 }
